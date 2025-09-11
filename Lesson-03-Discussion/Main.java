8d8d450b578be87747f7d3797824381b35907d44
tz-cloud <triciaz@nycstudents.net>
2025-09-11 13:17:38 +0000

    pieceofsh

--- Lesson-03-Discussion/Main.java
+++ Lesson-03-Discussion/Main.java
@@ -1,8 +1,8 @@
 class Main {
   public static void main(String[] args) {
     (new Main()).init();
-  }
-
+  }                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+                                                                                                                                                                                                            
   // Init function which runs when the 
   // program begins.  
 
