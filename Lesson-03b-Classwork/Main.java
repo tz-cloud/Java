f24bae6b3a61bcaa88f9b4255e8918fc8829ac05
tz-cloud <triciaz@nycstudents.net>
2025-09-15 12:58:44 +0000

    3rdwk

--- Lesson-03b-Classwork/Main.java
+++ Lesson-03b-Classwork/Main.java
@@ -9,8 +9,8 @@ class Main {
   void init(){
    
       System.out.println("Tricia Zhang");
-      System.out.println("Tricia Zhang"); System.out.println("144-50 35th Ave Flushing NY 11354");
-      System.out.println("Tricia Zhang \n 144-50 35th Ave Flushing NY 11354");
+      System.out.println("Tricia Zhang"); System.out.println("address");
+      System.out.println("Tricia Zhang \n address");
       
   }
 
