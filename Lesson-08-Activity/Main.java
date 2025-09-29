27fbb55fcfc13a902c0bfc46f031a82129e24089
tz-cloud <triciaz@nycstudents.net>
2025-09-29 13:16:25 +0000

    a8

---
+++ Lesson-08-Activity/Main.java
@@ -0,0 +1,23 @@
+class Main {
+
+	public static void main(String[] args) {
+    	(new Main()).init();
+	}
+
+  void init(){
+
+  }
+  
+  void type(String type){
+    System.out.println("type smth..");
+  }
+
+  String print(String type){
+		return type;
+	}
+
+
+
+	
+ 
+}
