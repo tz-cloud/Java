e3298e682ad3e8616808337fc0d24c865bba2ff2
tz-cloud <triciaz@nycstudents.net>
2025-09-29 13:32:58 +0000

    d8

--- Lesson-08-Activity/Main.java
+++ Lesson-08-Activity/Main.java
@@ -4,15 +4,18 @@ class Main {
     	(new Main()).init();
 	}
 
+	//1
 	void print(String word){
 		System.out.println(word);
 	}
 
+	//2
 	double FtoC(double f){
 		double c = (f-32)*9.0/5;
 		return c;
 	}
 
+	//testing
 	void init(){
 		print("Hello");
 		FtoC(32);
