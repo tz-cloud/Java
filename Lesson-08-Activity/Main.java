ecda260fc75eb9ba057b98982030ebfebbff9f25
tz-cloud <triciaz@nycstudents.net>
2025-10-01 00:47:19 +0000

    h8

--- Lesson-08-Activity/Main.java
+++ Lesson-08-Activity/Main.java
@@ -28,10 +28,10 @@ class Main {
 	}
 
 	//5
-	double distance(double x1, double y1, double x2, double y2){
+	void distance(double x1, double y1, double x2, double y2){
 		double d = Math.sqrt(Math.pow((x2 - x1) + (y2 - y1), 2));
-		return d;
 	}
+	
 	//testing
 	void init(){
 		print("Hello");
