5fdd15703f6d4a4e4e4d237c96ae8e528ec9e08f
tz-cloud <triciaz@nycstudents.net>
2025-10-01 00:43:19 +0000

    e8

--- Lesson-08-Activity/Main.java
+++ Lesson-08-Activity/Main.java
@@ -15,10 +15,30 @@ class Main {
 		return c;
 	}
 
+	//3
+	double sphereVolume(double r){
+		double v = (4/3.0)*Math.PI*Math.pow(r, 3);
+		return v;
+	}
+
+	//4
+	double coneVolume(double r, double h){
+		double v = Math.PI*Math.pow(r, 2)*(h/3.0);
+		return v;
+	}
+
+	//5
+	double distance(double x1, double y1, double x2, double y2){
+		double d = Math.sqrt(Math.pow((x2 - x1) + (y2 - y1), 2));
+		return d;
+	}
 	//testing
 	void init(){
 		print("Hello");
 		FtoC(32);
+		sphereVolume(1);
+		coneVolume(2, 3);
+		distance(1, 1, 1, 1);
   }
 
 
