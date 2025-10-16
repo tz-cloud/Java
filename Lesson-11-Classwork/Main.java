3ad1bc271497ad0be439d8c1e04fe368a311a6f3
tz-cloud <triciaz@nycstudents.net>
2025-10-16 13:32:32 +0000

    a11

---
+++ Lesson-11-Classwork/Main.java
@@ -0,0 +1,30 @@
+class Main {
+
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  String getUsername(String email){
+    return email;
+  }
+
+  String spiltReverse(String word){
+    return word;
+  }
+
+  String createUsername(String first, String second, int osis{
+    char l = 
+  }
+
+  void init(){
+   
+
+    
+  }
+
+  
+  
+}
