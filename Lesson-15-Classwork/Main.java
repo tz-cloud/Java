206ad0a783d1ea45193b2b8894eed4102a7774fa
tz-cloud <triciaz@nycstudents.net>
2025-10-24 13:32:32 +0000

    l1

---
+++ Lesson-15-Classwork/Main.java
@@ -0,0 +1,20 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  double approxPI(int term){
+    1/x
+  }
+
+  void init(){
+    
+
+  }
+
+  
+  
+  
+}
