97ae24aa2db47e59553ed9b11b0db09ae03ee2c4
tz-cloud <triciaz@nycstudents.net>
2025-10-26 00:43:45 +0000

    l2

--- Lesson-15-Classwork/Main.java
+++ Lesson-15-Classwork/Main.java
@@ -5,12 +5,15 @@ class Main {
   void print(Object o){ System.out.println(o);}
   void printt(Object o){ System.out.print(o);}
 
-  double approxPI(int term){
-    1/x
+  double approxPI(int N){
+    for(int x=N;x<=N;x++){
+      double approx = N*(1-1/3+1/5-1/7+1/9);
+    }
+    return approx;
   }
 
   void init(){
-    
+    print(5);
 
   }
 
