14928180c423fd04a4678f6b40a564c54d28ea6d
tz-cloud <triciaz@nycstudents.net>
2025-10-27 13:25:08 +0000

    m3

--- Lesson-15-Activity/Main.java
+++ Lesson-15-Activity/Main.java
@@ -7,6 +7,7 @@ class Main {
   void print(Object o){ System.out.println(o);}
   void printt(Object o){ System.out.print(o);}
 
+  //Problem 1
   String spaces(int N){
     String result = "";
     for(int x=0; x<=N; x++){
@@ -15,9 +16,21 @@ class Main {
     return result;
   }
 
+  //Problem 2
+  double harmonic(int N){
+    double sum = 0;
+    for(int x=0; x<=N; x++){
+      sum += 1/N;
+    }
+    return sum;
+  }
+
   void init(){
     System.out.println(spaces(3)+"DONE");
     System.out.println(spaces(5)+"DONE");
+
+    System.out.println(harmonic(1));
+    System.out.println(harmonic(2));
     
     
   }
