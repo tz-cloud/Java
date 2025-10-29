5e548c8f1400d7b3f232afb27c732918059a6eab
tz-cloud <triciaz@nycstudents.net>
2025-10-29 23:50:02 +0000

    m8

--- Lesson-15-Classwork/Main.java
+++ Lesson-15-Classwork/Main.java
@@ -8,10 +8,15 @@ class Main {
   double approxPI(int N){
     double approx = 0;
     for(int x=1;x<=N;x+=2){
-      approx += 4*(1-(1/-(x+2)));
+      double term = 1 / (2 * x + 1); 
+      if (x % 2 == 0){
+        approx += term;  //add
+      }else{
+        approx -= term;  //subtract
+      }
+      }
+      return 4*approx;
     }
-    return approx;
-  }
 
   void init(){
     print(approxPI(5));
