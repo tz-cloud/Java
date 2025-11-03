826a8d5efdaaf17be8ba6d95d97efcf87041bc0b
tz-cloud <triciaz@nycstudents.net>
2025-11-03 02:44:28 +0000

    o4

--- Lesson-17-Classwork/Main.java
+++ Lesson-17-Classwork/Main.java
@@ -1,3 +1,5 @@
+
+
 class Main {
   public static void main(String[] args) {
     (new Main()).init();
@@ -6,19 +8,49 @@ class Main {
   void printt(Object o){ System.out.print(o);}
 
   void init(){
+    System.out.println(swapLetter("bubblegum", "u", "i"));
+    //return bibblegim
+    System.out.println();
+    System.out.println(encryption3("abcdef"));
+    //return afbecd
 
 
     
   }
 
-  //will use reverse for problem 3
-  String reverse(String s){
-    String bld="";
-    for(int x=s.length()-1; x>=0; x--)
-      bld+=s.substring(x,x+1);
+  //PROBLEM 1
+  String swapLetter(String text, String swap, String replace){
+    String bld = "";
+        for (int x = 0; x < text.length(); x++){
+            String currentChar = text.substring(x, x + 1);
+            if(currentChar.equals(swap)){
+                bld += replace;
+            }else{
+                bld += currentChar;
+            }
+        }
+        return bld;
+  }
 
+  //PROBLEM 2
+  String encryption2(String text){
+    String bld= "";
+    int mid = text.length()/2;
+    String first = text.substring(0, mid);
+    String second = text.substring(mid);
+    for(int x=0; x < text.length(); x++){
+      //idk man
+    }
     return bld;
   }
 
-
+  //PROBLEM 3
+  String encryption3(String text){
+    String bld = "";
+    for(int x = 0; x < text.length(); x++){
+      int last = text.length()-x;
+      bld += text.substring(x,last);
+    }
+    return bld;
+  }
 }
