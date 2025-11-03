646480f23f7c87a09a1eae5c2ddbf8a985b1388e
tz-cloud <triciaz@nycstudents.net>
2025-11-03 02:22:15 +0000

    idk

--- Lesson-17-Classwork/Main.java
+++ Lesson-17-Classwork/Main.java
@@ -14,33 +14,11 @@ class Main {
   //will use reverse for problem 3
   String reverse(String s){
     String bld="";
-    for(int x=s.length()-1; x>=0; x--) //going backwards
+    for(int x=s.length()-1; x>=0; x--)
       bld+=s.substring(x,x+1);
 
     return bld;
   }
 
-  //PROBLEM 1
-  String swapLetter(String s, char swap, char replace){
-    String bld="";
-    for(int x = 0; x < s.length(); x++){
-      if(s.substring(x, x+1).equals("swap")){
-        swap = replace;
-      }
-    }
-    return bld;
-  }
-
-  //PROBLEM 2
-  String encryption2(String s){
-    String bld="";
-    return bld;
-  }
-
-  //PROBLEM 3
-  String encryption3(String s){
-    String bld="";
-    return bld;
-  }
 
 }
