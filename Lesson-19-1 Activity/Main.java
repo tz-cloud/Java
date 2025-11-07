c94c005faf24a3969453333e4f563fd6612136b1
tz-cloud <triciaz@nycstudents.net>
2025-11-07 14:21:27 +0000

    p7

--- Lesson-19-1 Activity/Main.java
+++ Lesson-19-1 Activity/Main.java
@@ -3,9 +3,6 @@ class Main {
   public static void main(String[] args) {
     (new Main()).init();
   }
-  
-  void print(Object o){System.out.println(o);}
-  void printt(Object o){System.out.print(o);}
 
   //PROBLEM 1
   String rollDice(){
