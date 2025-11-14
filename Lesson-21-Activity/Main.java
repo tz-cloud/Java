fa3c629ccedfe8edf73d8afd708e1574eb501c8b
tz-cloud <triciaz@nycstudents.net>
2025-11-14 14:05:16 +0000

    q2

--- Lesson-21-Activity/Main.java
+++ Lesson-21-Activity/Main.java
@@ -7,18 +7,30 @@ class Main {
   void print(Object o){System.out.println(o);}
   void printt(Object o){System.out.print(o);}
 
-  double allowance(){
-    String days = "";
+  String allowance(){
+    int days = 0;
     double penny = 0.01;
     double amount = penny*2;
     while(!(amount==5)){
       days++;
     }
-    return days;
+    return "It would take " + days + " to reach $5.";
+  }
+
+  int addTwoNumbers(){
+    int num1 = randInt(1,100);
+    int num2 = randInt(1,100);
+    int equ = num1 + num2;
+    System.out.println(equ + "=?");
+    int guess = Input.readInt();
+    while()
+
+
+
   }
 
   void init(){
-    
+    allowance();
   }
 
 
