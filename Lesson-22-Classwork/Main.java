9019837183326fd0de6ca0e6463ce15dc7ad9769
tz-cloud <triciaz@nycstudents.net>
2025-11-18 01:51:11 +0000

    q4

---
+++ Lesson-22-Classwork/Main.java
@@ -0,0 +1,27 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+  
+  void init(){
+   
+    String[] friends = {"Ashley","Emma","Kim","Brian","Leon"};
+    double[] c = {0, 12, 30};
+    System.out.println(convert(c));
+  }
+
+  double[] convert(double[] c){
+
+    double[] converted = new double[c.length];
+    for(int i=0; i < c.length; i++){
+      converted[i] = celsiusToFarhenheit(c[i]);
+    }
+    return converted;
+  }
+
+  double celsiusToFarhenheit(double c){
+        return c*9.0/5.0+32;
+  }
+}
