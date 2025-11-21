b9423db015065859fcab41a4376162b97e7730a4
tz-cloud <triciaz@nycstudents.net>
2025-11-21 13:10:41 +0000

    r1

---
+++ Lesson-23-Activity/Main.java
@@ -0,0 +1,49 @@
+class Main {
+
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  
+  void print(Object o){System.out.println(o);}
+  void printt(Object o){System.out.print(o);}
+
+  double total(double[] prices){
+    double sum = 0;
+    for(int i = 0; i < prices.length; i++){
+      sum += prices[i];
+    }
+    return sum*1.08525;
+  }
+
+  void init(){
+    double[] price = {2, 8, 3.99, 10};
+    print(total(price));
+
+  }
+
+
+
+  // Use these functions below for the activity
+
+  String convertToEmoji(String code){
+    return String.valueOf(Character.toChars(
+                               Integer.parseInt(code, 16)));
+  }
+
+  
+  int randInt(int lower, int upper){
+    int range = upper - lower;
+    return (int)(Math.random()*range+lower);
+  }
+
+  boolean isPrime(int n){
+    int divisor = 2;
+    while(n%divisor !=0){
+      divisor+=1;
+    }
+    if(divisor==n)
+      return true;
+    else
+      return false;
+  }
+}
