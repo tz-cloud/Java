a10f1badca87592229cf960a011508e09f26df2e
tz-cloud <triciaz@nycstudents.net>
2025-11-24 23:09:04 +0000

    r6

---
+++ Lesson-24-Classwork/Main.java
@@ -0,0 +1,41 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  String toUpper(String msg){
+    String bld = "";
+    for(int i = 0; i < msg.length(); i++){
+      char ch = msg.charAt(i); //to convert String into character
+      int ascii = ch-32;
+      bld += (char)ascii;
+    }
+    return bld;
+  }
+
+  String genPswd(){
+    String pswd = "";
+    for(int i = 0; i < 7; i++){
+      pswd += randInt(i, i);
+    }
+    return pswd;
+  }
+
+  void init(){
+    print(toUpper("hello world"));
+    //HELLO WORLD
+    print(genPswd());
+  }
+
+
+
+   
+  int randInt(int lower, int upper){
+    int range = upper - lower;
+    return (int)(Math.random()*range+lower);
+  }
+
+ 
+}
