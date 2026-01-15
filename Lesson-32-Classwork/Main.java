2008760536054990f903d25e1b2153d329842ddf
tz-cloud <triciaz@nycstudents.net>
2026-01-15 14:29:59 +0000

    washington

---
+++ Lesson-32-Classwork/Main.java
@@ -0,0 +1,36 @@
+class Main {
+  public static void main(String[] args) {
+    (new Main()).init();
+  }
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  void init(){
+
+    double[] g1 = {60,70,90};
+    double[] g2 = {65,75,60};
+
+    String[]  courses1  = {"EEFG7QH","FSFC3","HQFC3TH","HUF43X"};
+    String[]  courses2  = {"MRF43","PGF43Q3C","SCF43QG","HUF43X"};
+
+    
+    //Create a new student objects 
+    Student s1 = new Student("Tom", 12, 3.4, g1, courses1);
+    Student s2 = new Student("Jerry", 11, 3.9, g2, courses2);
+
+    s1.calculateAvg();
+    s1.checkCourse(courses1);
+    s2.calculateAvg();
+    s2.checkCourse(courses2);
+
+
+  }
+
+
+  
+  int randInt(int lower, int upper){
+    int range = upper - lower;
+    return (int)(Math.random()*range+lower);
+  }
+
+}
---
+++ Lesson-32-Classwork/Student.java
@@ -0,0 +1,38 @@
+class Student{
+
+  String firstName;
+  int gradeLevel;
+  double gpa;
+  double[] grades;
+  String[] courses;
+
+  
+  Student(String firstName, int gradeLevel, double gpa, 
+          double[] grades, String[] courses){
+    this.firstName  = firstName;
+    this.gradeLevel = gradeLevel;
+    this.gpa        = gpa;
+    this.grades     = grades;
+    this.courses    = courses;
+  }
+
+  // class functions
+
+  double calculateAvg(){
+    double sum=0;
+    for(int x=0; x<=this.grades.length-1;x++){
+      sum+=this.grades[x];
+    }
+    return sum/this.grades.length;
+  }
+
+  boolean checkCourse(String[] courses){
+    if(courses.equals("HUF43X")){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  
+}
