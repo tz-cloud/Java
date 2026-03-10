f2cabb7183c5082fc4c678ef410a045adbcf1e75
tz-cloud <triciaz@nycstudents.net>
2026-03-10 13:28:33 +0000

    done3

--- Lesson-40-Activity/Main.java
+++ Lesson-40-Activity/Main.java
@@ -18,10 +18,18 @@ class Main {
    
     String queryResult="";
     String sql ="Select * From cr101 Where teacher1 = 'BANU';";
-
+    String sql1 ="Select * From cr101 Where teacher1 = 'PORCHETTA' And Period = '10';";
+    String sql2 ="Select studentId From cr101;";
+    String sql3 ="Select studentId,course, period From cr101 Where room In('322', '105', '106', '323') And Grade = '11';";
+    String sql4 ="Select * From cr101 Where course LIKE 'M%' And course LIKE '%X';";
     Database db =  new  Database("jdbc:sqlite:students.db");	
 
     queryResult = db.runSQL(sql, "table-auto");
+    queryResult = db.runSQL(sql1, "table-auto");
+    queryResult = db.runSQL(sql2, "table-auto");
+    //there are 34,731 students
+    queryResult = db.runSQL(sql3, "table-auto");
+    queryResult = db.runSQL(sql4, "table-auto");
     print(queryResult); 
 
     
