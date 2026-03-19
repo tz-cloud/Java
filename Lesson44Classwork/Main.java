2c52b7c0f86689e4fcf0b7252fbec1c89cb6aed6
tz-cloud <triciaz@nycstudents.net>
2026-03-19 12:27:17 +0000

    half

---
+++ Lesson44Classwork/Main.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+//For compiling on the shell on repl: Same on mac
+//javac -cp sqlite-jdbc-3.23.1.jar: Main.java
+
+//Use for windows
+//javac -cp sqlite-jdbc-3.23.1.jar; ServerExample.java
+class Main {
+
+ public static void main(String[] args)throws IOException{
+    (new Main()).init();
+  }
+
+  void print(Object o){ System.out.println(o);}
+  void printt(Object o){ System.out.print(o);}
+
+  void init() {
+
+    String result="";
+    String sql = "SELECT albums.title FROM albums INNER JOIN artists ON albums.ArtistId = artists.ArtistId limit 20;";
+    String sql1 ="SELECT tracks.name FROM tracks INNER JOIN genres ON tracks.genreld = genres.genreld WHERE genres.genreld IN('Blues', 'Latin', 'Pop', 'Classical') limit 20;";
+    String sql2 = "SELECT tracks.name FROM tracks INNER JOIN playlist_track ON tracks.TrackId = playlist_track.TrackId INNER JOIN playlists ON playlist_track.PlaylistId = playlists.PlaylistId WHERE playlist.name = 'Grunge' limit 20;";
+     
+		Database db = new Database("jdbc:sqlite:chinook.db" );	
+    result = db.runSQL(sql, "table-auto");
+    print(result); 
+
+    
+
+      
+  }    
+}
