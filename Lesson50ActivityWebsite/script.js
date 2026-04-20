70532bd2ccfd55684d696c81c2729cad174cc22d
tz-cloud <triciaz@nycstudents.net>
2026-04-20 02:26:11 +0000

    ?

--- Lesson50ActivityWebsite/script.js
+++ Lesson50ActivityWebsite/script.js
@@ -1,13 +1,22 @@
 
-let data,songs;
+let data, songs, customers;
 function init(){
-  $.ajaxSetup({async: false});
+  $.ajaxSetup({async: true});
   
   let link = "https://opulent-memory-g479qpvjg4vrfwq59-8500.app.github.dev/"; 
 	// link is from the port that is running the DB server
 	let route = "customer";
 	// the route link already has a / in front so we take it out of route
-	console.log(data)
+	customers = await info.json();
+	let output = document.getElementById(output);
+	let bld = "";
+  for(let x = 0; customers.length; x++){
+		let customer = customers[x];
+		bld += `<h3> Customer Id: ${customer.CustomerId} </h3>`;
+		//case sensitive in java
+		bld += `<div> Customer First Name: ${customer.FirstName} </div>`;
+}
+
 
  
 }
